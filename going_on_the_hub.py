c27fea67b17c534a2ba0d5969687ce19bbb2e787
Snoddis <oddionm@gmail.com>
2023-01-31 22:48:43 +0100

    tror dette gir lampe i Snoddis

--- going_on_the_hub.py
+++ going_on_the_hub.py
@@ -10,10 +10,11 @@ def going_on_the_hub():
     print("You see a lot of people")
     print("You see a lot of people")
     print("You see a lot of people")
+    print("You see a lot of people in a ditch")
+    print("You see a lot of people at home")
     print("You see a lot of people")
     print("You see a lot of people")
     print("You see a lot of people")
-    print("You see a lot of people")
-
+    print("lampe")
 going_on_the_hub()
 
