ac913b5828500e8ded12a06108ee22ce255648b8
Snoddis <oddionm@gmail.com>
2023-01-31 22:31:11 +0100

    New Commit from different branch or smthn

---
+++ going_on_the_hub.py
@@ -0,0 +1,19 @@
+#make some fancy function and call it
+
+def going_on_the_hub():
+    print("You are going on the hub")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+    print("You see a lot of people")
+
+going_on_the_hub()
+
